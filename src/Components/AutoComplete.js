142fa90bb2107430bc83dea79dcd0b662eb778aa
Aaron <aaronlo826@gmail.com>
2022-05-09 16:37:32 -0400

    base files for autcomplete.js

---
+++ src/Components/AutoComplete.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
+const AutoComplete = () => {
+  const [autoFill, setAutoFill] = useState([]);
+  useEffect ( () => {
+    axios({
+
+    }).then((response) =>{
+
+
+
+    })
+
+
+
+  })
+
+
+
+  return(
+    <section>
+
+    </section>
+  )
+}
