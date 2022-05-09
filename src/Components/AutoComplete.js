d1a48ccb7ef80b18a41edd8e8f0a8a59dff3db4f
Victoria <victoriareis_@outlook.com>
2022-05-09 17:49:13 -0400

    finished autocomplete api call

--- src/Components/AutoComplete.js
+++ src/Components/AutoComplete.js
@@ -1,27 +1,58 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-
 const AutoComplete = () => {
-  const [autoFill, setAutoFill] = useState([]);
-  useEffect ( () => {
-    axios({
+	const [autoFill, setAutoFill] = useState("");
+	const [suggestions, setSuggestions] = useState([]);
+	useEffect(() => {
+		axios({
+			url: "https://api.datamuse.com/sug",
+			method: "GET",
+			dataResponse: "JSON",
+			params: {
+				s: autoFill,
+				max: 10,
+			},
+		}).then((response) => {
+			setSuggestions(response.data);
+			console.log(response.data);
+		});
+	}, [autoFill]);
 
-    }).then((response) =>{
+	const handleChange = (event) => {
+		setAutoFill(event.target.value.trim());
+		console.log(event.target.value);
+	};
 
+	return (
+		<section>
+			<form>
+				<label htmlFor="search">Search</label>
 
+				<input
+					type="text"
+					id="search"
+					onChange={(event) => {
+						handleChange(event);
+					}}
+					// value={""}
+				/>
 
-    })
+				<button type="submit">Submit</button>
+			</form>
+			<ul>
+				{autoFill !== ""
+					? suggestions.map((wordObj, index) => {
+							return (
+								<li key={index} onClick={() => {}}>
+									{wordObj.word}
+								</li>
+							);
+					  })
+					: null}
+			</ul>
+		</section>
+	);
+};
 
-
-
-  })
-
-
-
-  return(
-    <section>
-
-    </section>
-  )
-}
+export default AutoComplete;
