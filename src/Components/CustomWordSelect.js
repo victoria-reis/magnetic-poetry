57419c36438b494ff784a7c0dbf63de5715b5a08
Aaron <aaronlo826@gmail.com>
2022-05-14 14:08:46 -0400

    custom words added

---
+++ src/Components/CustomWordSelect.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+const CustomWordSelect = ({wordPoem, setWordPoem}) => {
+  const [customInput, setCustomInput] = useState("");
+  const [customTextBox, setCustomTextBox] = useState([]);
+
+  const handleCustomSubmit = (e) => {
+    e.preventDefault();
+    setCustomTextBox([... customTextBox, customInput]);
+  }
+
+  const handleCustomChange = (e) => {
+    setCustomInput(e.target.value.trim());
+
+  }
+  const handleCustomSelection = (customWord) => {
+    console.log(customWord)
+    setWordPoem([wordPoem + customWord + " "]);
+  }
+
+  console.log (customTextBox)
+
+  return (
+    <div>
+      {
+      customTextBox.map((customWord, index) => {
+        return (
+          <div key={index} onClick={() => handleCustomSelection(customWord)}>
+            {customWord}
+          </div>
+        );
+      })
+      }
+
+      <form
+      onSubmit={(event) => {
+        handleCustomSubmit(event);
+      }}
+      >
+      <label htmlFor="search">Custom text</label>
+      <input
+      type="search"
+      id="search"
+      onChange={(e) => {
+        handleCustomChange(e);
+      }}
+      value={customInput}
+      />
+      <button type="submit">Add</button>
+      </form>
+
+    </div>
+    )
+};
+
+export default CustomWordSelect
