a6b7f64831292636057bd59ae07dda1c2d012d8d
Gigi <yuengepoon@gmail.com>
2022-05-17 19:05:13 -0400

    merging and added title

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,14 @@
 const Footer = () => {
-	return (
-		<div className="footer">
-			<p> Made by Aaron, Gigi, Slava, Victoria at Juno College 2022</p>
-			<a href="https://www.freepik.com/vectors/watercolor-wallpaper">
-				Watercolor wallpaper vector created by Freepik - www.freepik.com
-			</a>
-		</div>
-	);
-};
+    
+            return (
+                    <div className="footer">
+                        <p> Made by Aaron, Gigi, Slava, Victoria at Juno College 2022</p>
+                        <a href="https://www.freepik.com/vectors/watercolor-wallpaper">Watercolor wallpaper vector created by freepik - www.freepik.com </a>
+                        | 
+                        <a href="https://www.freepik.com/vectors/border"> Border vector created by pch.vector - www.freepik.com</a>
+                    </div>
+                
+            );
+}
 
 export default Footer;
